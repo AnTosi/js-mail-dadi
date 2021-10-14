afa4a1d929a867dff9c3fa644a513fc0c0891342
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-14 15:36:47 +0200

    aggiustato problema nell'event listener che restituiva il placeholder

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,21 @@
 //const varie
 //array (lista mail del mio elenco)
 //bottone input per inserire mail
+//event listener per "dire" al sistema che ho inserito la mail
 //if mail inserita è in elenco ---> esito positivo, else -----> esito negativo
 
-const mailList = ["queutricaweicre-6784@yopmail.com", "quacrappeullaho-9067@yopmail.com", "xoidexatrefe-8363@yopmail.com", "mail@fabiopacifici.com", "hello@tizianonicolai.com"];
+const mailList = ["queutricaweicre-6784@yopmail.com", "quacrappeullaho-9067@yopmail.com",               "xoidexatrefe-8363@yopmail.com", "mail@fabiopacifici.com", "hello@tizianonicolai.com", "cane", "fabiola.gardin.85@gmail.com"];
+
+
+
+// secondo me così dovrebbe funzionare ma mi stampa il placeholder invece che il contenuto
+document.getElementById("click").addEventListener ("click", function(){
+    const userMail = document.getElementById("userMail");
+    const mailValue = userMail.value;
+    console.log(mailValue);
+})
+
+
 
 
 

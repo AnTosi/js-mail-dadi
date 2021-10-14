4beeb11e541b605fe2b6aed27f4f7bce02a92957
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-14 15:06:15 +0200

    creati file, collegati, iniziati commenti e creato array

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// esercizio in 2 fasi:
+
+//** fase 1: Mail  
+//chiedi all'utente mail e controlla se rientra nella lista (array)
+//se rientra nella lista messaggio esito positivo, sennò negativo
+//
+//strumenti:
+//const varie
+//array (lista mail del mio elenco)
+//if mail inserita è in elenco ---> esito positivo, else -----> esito negativo
+
+const mailList = ["queutricaweicre-6784@yopmail.com", "quacrappeullaho-9067@yopmail.com", "xoidexatrefe-8363@yopmail.com", "mail@fabiopacifici.com"];
+
+
+
+    

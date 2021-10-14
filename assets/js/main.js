8e77db35f3a5fbcb30cc22c778580f223a5e9fcc
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-14 16:07:12 +0200

    sono bloccato perché array is undefined

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,39 @@
 //event listener per "dire" al sistema che ho inserito la mail
 //if mail inserita è in elenco ---> esito positivo, else -----> esito negativo
 
-const mailList = ["queutricaweicre-6784@yopmail.com", "quacrappeullaho-9067@yopmail.com",               "xoidexatrefe-8363@yopmail.com", "mail@fabiopacifici.com", "hello@tizianonicolai.com", "cane", "fabiola.gardin.85@gmail.com"];
+let mailList = ["queutricaweicre-6784@yopmail.com", "quacrappeullaho-9067@yopmail.com",               "xoidexatrefe-8363@yopmail.com", "mail@fabiopacifici.com", "hello@tizianonicolai.com", "cane", "cane@gmail.com", "fabiola.gardin.85@gmail.com"];
+console.log(mailList.length);
 
 
 
 // secondo me così dovrebbe funzionare ma mi stampa il placeholder invece che il contenuto
+// risolto con value
 document.getElementById("click").addEventListener ("click", function(){
     const userMail = document.getElementById("userMail");
     const mailValue = userMail.value;
     console.log(mailValue);
 })
+
+// metto la funzione per vedere se array contiene mail inserita
+document.getElementById("click").addEventListener ("click", function containsObject(mailValue, mailList) {
+    for (var i = 0; i < mailList.length; i++) {
+        if (mailList[i] === mailValue) {
+            console.log("bentornato");
+        } else {
+        console.log("non puoi entrare");
+        }
+    }
+}
+)
+
+//mi sono bloccato qua sopra perché mi dà mailList ins undefined
+
+
+
+// metto l'if, potevo concatenarlo e sarebbe stato meglio ma già mi intrico così
+
+// non so se mettere due event listener, che uno mi serve sopra e uno sotto e non credo funzioni se li accorpo, ma non so se ne metto due se con un click me li attiva entrambi, forse dovrei concatenarlo (o mi ritrovo con la funzione definita dopo, o mi ritrovo con la costante definita dopo)
+
 
 
 
